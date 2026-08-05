2caf5938a8bef8616eaeace8f1656e6c73ba2bd6
Jérémie Levy <jeremylevy.131290@gmail.com>
2026-08-05 10:36:35 +0200

    feat: initial commit and global setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="w-fit text-xl text-green-600 font-bold">
+        <p className="font-mono tracking-wider text-3xl text-nuage tracking-widder">jeremie.io</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+// Fira Sans
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/600.css'
+import '@fontsource/fira-sans/700.css'
+
+// Azeret Mono
+import '@fontsource/azeret-mono/400.css'
+import '@fontsource/azeret-mono/500.css'
+
+// Cascadia Code
+import '@fontsource/cascadia-code/400.css'
+import '@fontsource/cascadia-code/400-italic.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
